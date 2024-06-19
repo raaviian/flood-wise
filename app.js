29f9925226b6868aac0a1c95eda7a9e130575bdb
Raviansyah Noorman <90436146+raaviian@users.noreply.github.com>
2024-06-19 21:58:51 +0800

    fix: database migration

--- app.js
+++ app.js
@@ -72,10 +72,11 @@ function sendMassEmail(subject, text) {
 
 // MySQL database connection pool
 const connection = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "iot-floodwise",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   connectionLimit: 10, // Connection pool limit
 });
 
